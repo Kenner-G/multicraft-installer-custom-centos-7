cfcc7a20e6328fb7ab220eb8f1f13d076d698e63
Kenner-G <kenner.galdamez@gmail.com>
2016-12-27 09:45:58 -0700

    User Agreement

--- mc-installer.sh
+++ mc-installer.sh
@@ -9,20 +9,22 @@
 # *
 # **/
 
-echo
-echo "***"
-echo "*** Welcome to Multicraft 7 centos install script! (Custom)"
-echo "***"
-echo
-echo "This installer will help you get Multicraft up and running also install all the packages and dependencies."
-echo "No changes are made to the system until all of the required information has been collected."
-echo
-echo "NOTE: This script automates the installation as described on the Multicraft website. Use it at your own risk."
-echo "Custom coded and edited by David Brockway."
-echo
-echo
+read -p "Do you wish to install Multicraft? (y/n) Please keep in mind that we provide no support for this script. Report any issues on our GitHub repo. (y/n)" CONT
+if [ "$CONT" = "y" ]; then
+ echo
+ echo "***"
+ echo "*** Welcome to Multicraft 7 centos install script! (Custom)"
+ echo "***"
+ echo
+ echo "This installer will help you get Multicraft up and running also install all the packages and dependencies."
+ echo "No changes are made to the system until all of the required information has been collected."
+ echo
+ echo "NOTE: This script automates the installation as described on the Multicraft website. Use it at your own risk."
+ echo "Custom coded and edited by David Brockway."
+ echo
+ echo   
 
-echo "Updating your centos system os1"
+ echo "Updating your centos system os1"
 
  sleep 1
 
@@ -30,84 +32,84 @@ echo "Updating your centos system os1"
 
 ### Installation
 
-echo "Installing httpd, unzip, zip"
+ echo "Installing httpd, unzip, zip"
  sleep 1
    yum -y install httpd unzip zip nano
 
 
-echo "Starting Apache"
+ echo "Starting Apache"
  sleep 1
   sudo systemctl start httpd
   
-echo "Enabling Apache"
+ echo "Enabling Apache"
  sleep 1
   sudo systemctl enable httpd
   
-echo "Checking the status of Apache"
+ echo "Checking the status of Apache"
  sleep 1
   sudo Checking the status of Apache
   
-echo "Apache has been isnatlled now installing Mysql-mariadb"
+ echo "Apache has been isnatlled now installing Mysql-mariadb"
  sleep 1
   sudo yum -y install mariadb-server mariadb
   
-echo "Starting Mysql-mariadb"
+ echo "Starting Mysql-mariadb"
  sleep 1
   sudo systemctl start mariadb
 
-echo "Enabling Mysql-mariadb"
+ echo "Enabling Mysql-mariadb"
  sleep 1
   sudo systemctl enable mariadb
 
-echo "Checking the status of Mysql-mariadb"
+ echo "Checking the status of Mysql-mariadb"
  sleep 1
   sudo systemctl status mariadb
   
-echo "Starting Mysql-mariadb secure setup, user should enter the details they want for the MYSQL Server"
+ echo "Starting Mysql-mariadb secure setup, user should enter the details they want for the MYSQL Server"
  sleep 1
   sudo sudo mysql_secure_installation
   
-echo "MYSQL Servers has been installed now installing PhpMyAdmin"
+ echo "MYSQL Servers has been installed now installing PhpMyAdmin"
  sleep 1
   yum install epel-release
   yum install phpmyadmin
 
-echo "PhpMyAdmin have been installed now Add # to Require ip 127.0.0.1 and Require ip ::1
-Add Require all granted. Then save. Ctrl + X then Y then [Enter]"
+ echo "PhpMyAdmin have been installed now Add # to Require ip 127.0.0.1 and Require ip ::1
+ Add Require all granted. Then save. Ctrl + X then Y then [Enter]"
  sleep 5
   sudo nano /etc/httpd/conf.d/phpMyAdmin.conf
 
-echo "Installing dependencies"
+ echo "Installing dependencies"
  sleep 1
   yum install php php-mysql php-gd php-pear php-common php-mbstring php-ldap php-odbc php-xml php-xmlrpc php-bcmath php-mysql php-pdo wget vim tar zip curl java-1.8.0-openjdk
   yum install httpd php php-common php-gd php-mbstring php-ldap php-odbc php-pear php-xml php-xmlrpc php-bcmath php-mysql php-pdo wget vim tar zip curl java-1.8.0-openjdk
 
-echo "dependencies have been installed now restarting Apache"
+ echo "dependencies have been installed now restarting Apache"
  sleep 2
   sudo systemctl restart httpd.service
 
-echo "You now need to Replace All: AllowOverride None with AllowOverride All. (Total 4 to change)
+ echo "You now need to Replace All: AllowOverride None with AllowOverride All. (Total 4 to change)
 Then save. Ctrl + X then Y then [Enter]
 "
  sleep 5
   sudo nano /etc/httpd/conf/httpd.conf
 
 
-echo "Restarting apache"
+ echo "Restarting apache"
  sleep 1
   sudo systemctl restart httpd.service
 
 
-echo "Disabling setenforce"
+ echo "Disabling setenforce"
  sleep 1
   sudo  setenforce 0
   
-echo "Downloading Multicraft"
+ echo "Downloading Multicraft"
  sleep 1
   wget --content-disposition http://www.multicraft.org/download/index?arch=linux64
 
 
-echo "Extracting multicraft"
+ echo "Extracting multicraft"
  sleep 2
   tar -xzf multicraft*.tar.gz
 
@@ -116,20 +118,20 @@ echo "Extracting multicraft"
 
 
 
-echo "Running multicraft install script"
+ echo "Running multicraft install script"
  sleep 4
   sudo ./setup.sh
 
-echo "Now Create Database
+ echo "Now Create Database
 Open Internet Browser and go to http://your_server_ip/Phpmyadmin, then Login with Username: root Password: Your created (MySQL) password.
 Open Tab database tab in phpmyadmin. Then create database: multicraft_panel and multicraft_daemon"
  sleep 1
 
-echo "Multicraft Install.php
+ echo "Multicraft Install.php
 Continue the setup in http://your_server_ip/html/multicraft/install.php"
  sleep 4
  
-echo "Multicraft Install.php instructions, 
+ echo "Multicraft Install.php instructions, 
 1. Press Continue.
 2. Press Continue Again if Configuration File Found.
 3. Setup the Panel MySQL - View Press Save, Then Initialize Database, ThenContinue.
@@ -140,28 +142,32 @@ echo "Multicraft Install.php instructions,
 8. Open Terminal - (PuTTy) and start the Daemon(s)."
  sleep 5
 
-echo "Starting multicraft daemon"
+ echo "Starting multicraft daemon"
  sleep 1
   sudo /home/minecraft/multicraft/bin/multicraft start
   
-echo
-echo "***"
-echo "*** You have successfully install Multicraft on centos 7"
-echo "***"
-echo
-echo "This installer will help you get Multicraft up and running also install all the packages and dependencies."
-echo "No changes are made to the system until all of the required information has been collected."
-echo
-echo "NOTE: Thank you for using the custom script to install multicraft centos 7.
-echo "We would we would appreciate if you could share this with your friends to help
-and support this project. Also it shows how easy multicraft is to install on centos 7 with this 
-custom script."
-echo "Created by David Brockway and Kenner, if you would like to contribute to this project you can do so via my github 
-link https://goo.gl/v8jYNT"
-echo
-echo
+ echo
+ echo "***"
+ echo "*** You have successfully install Multicraft on centos 7"
+ echo "***"
+ echo
+ echo "This installer will help you get Multicraft up and running also install all the packages and dependencies."
+ echo "No changes are made to the system until all of the required information has been collected."
+ echo
+ echo "NOTE: Thank you for using the custom script to install multicraft centos 7.
+ echo "We would we would appreciate if you could share this with your friends to help
+ and support this project. Also it shows how easy multicraft is to install on centos 7 with this 
+ custom script."
+ echo "Created by David Brockway and Kenner, if you would like to contribute to this project you can do so via my github 
+ link https://goo.gl/v8jYNT"
+ echo
+ echo
  
 
 echo "Exiting MultiCraft centos 7 installer script!"
  sleep 1
   quit
+else
+  echo "Oh well, you don't want to use our installer. See ya!";
+  cd ~
+fi
